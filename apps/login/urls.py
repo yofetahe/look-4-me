5b71dbbe286670a440774053e915be0c986d5dac
Qianye Zhang <helloqyzhang@gmail.com>
2019-02-26 00:38:11 -0800

    html

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.login, name="login"),
+    url(r'^homepage$',views.homepage),
+    url(r'^search$',views.search),
+    # url(r'^search_process$',views.search_process),
+    # url(r'^posts$', views.post, name='posts'),
+    url(r'^search/(?P<userid>[0-9]+)$',views.user_info_display),
+    url(r'^message$',views.message_like),
 ]
--- apps/login/views.py
+++ apps/login/views.py
@@ -2,3 +2,39 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'login/index.html')
+
+def homepage(request):
+    return render(request,'login/homepage.html')
+
+
+def search(request):
+    return render(request,'login/search.html')
+
+
+# def search_process(request):
+#     context = {
+#         'posts': Post.objects.all(),
+#         'new_post_form' : PostForm()
+#     }
+#     return render(request, 'login/search.html', context)
+
+
+# def post(request):  
+#     if request.method == 'POST':
+#         bound_form = PostForm(request.POST)
+ 
+#     if bound_form.is_valid():
+#         Post.objects.create(description=request.POST['description'])
+#     context = {
+# 		'posts': Post.objects.all()
+#     }
+#     return render(request, 'login/search_result.html', context)
+
+
+def user_info_display(request,userid):
+    return render(request,'login/user_info.html',)
+
+
+def message_like(request):
+    return render(request,'login/message_like.html',)
+
