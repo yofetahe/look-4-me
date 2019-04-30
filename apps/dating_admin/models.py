e2c64013c870925f8dd12dcd7c8fb29fdc01f470
Yofatehe <42419407+yofetahe@users.noreply.github.com>
2019-04-30 11:20:42 -0700

    change on dating-admin model - on_delete=Protect

--- apps/dating_admin/models.py
+++ apps/dating_admin/models.py
@@ -8,12 +8,12 @@ class Category(models.Model):
 class Questionnaire(models.Model):
     question_content = models.TextField()
     question_type = models.CharField(max_length=15)  #-- 1, 2, 11, 22
-    category = models.ForeignKey(Category, related_name='questions')
+    category = models.ForeignKey(Category, related_name='questions', on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Choice(models.Model):
     choice_content = models.CharField(max_length=255)
-    questions = models.ForeignKey(Questionnaire, related_name='choices')
+    questions = models.ForeignKey(Questionnaire, related_name='choices', on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
